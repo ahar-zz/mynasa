e14e50a127bd8307065e5916fa033b30c3743e75
renat <renat.kabirov@bookedin.net>
2019-01-09 14:15:08 -0600

    fixes

--- src/com/nasa/app/NEOApp.java
+++ src/com/nasa/app/NEOApp.java
@@ -1,67 +1,26 @@
 package com.nasa.app;
 
-import com.nasa.app.constants.AppConstants;
-import com.nasa.app.factory.AsteroidFactory;
 import com.nasa.app.helper.UrlFormatterUtil;
 import com.nasa.app.models.Asteroid;
-import com.nasa.app.exceptions.AsteroidParsingException;
 import com.nasa.app.services.AsteroidService;
 import com.nasa.app.services.NasaService;
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 
 import javax.json.*;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-import java.security.GeneralSecurityException;
-import java.security.cert.X509Certificate;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class NEOApp {
-    private final static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-    private final static NasaService nasaService = new NasaService();
-    private final static AsteroidFactory asteroidFactory = new AsteroidFactory();
-    private final static AsteroidService asteroidService = new AsteroidService();
-    /**
-     * Method to print out url response.
-     *
-     * @param content - byte array to print.
+    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+    private final NasaService nasaService;
+    private final AsteroidService asteroidService;
+
+    /*
+     * Constructor for NEOApp
      */
-    private static void printURLResponce(byte[] content) {
-        System.out.println("/***** URL Content *****\n");
-        System.out.println(new String(content));
+    public NEOApp(NasaService nasaService, AsteroidService asteroidService) {
+        this.nasaService = nasaService;
+        this.asteroidService = asteroidService;
     }
-
-    /**
-     * Method to trust all certificates, to prevent error connected with URL connection to https.
-     */
-    private static void trustAllCerts() {
-        TrustManager[] trustAllCerts = new TrustManager[]{
-                new X509TrustManager() {
-                    public X509Certificate[] getAcceptedIssuers() {
-                        return new X509Certificate[0];
-                    }
-
-                    public void checkClientTrusted(
-                            X509Certificate[] certs, String authType) {
-                    }
-
-                    public void checkServerTrusted(
-                            X509Certificate[] certs, String authType) {
-                    }
-                }
-        };
-
-        try {
-            SSLContext sc = SSLContext.getInstance("SSL");
-            sc.init(null, trustAllCerts, new java.security.SecureRandom());
-            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-        } catch (GeneralSecurityException e) {
-        }
-    }
-
 
     /**
      * Main method for retrieving data and printing it out.
@@ -69,10 +28,19 @@ public class NEOApp {
      * @param args
      */
     public static void main(String[] args) {
+        NasaService nasaService = new NasaService();
+        AsteroidService asteroidService = new AsteroidService();
+
+        NEOApp neoApp = new NEOApp(nasaService, asteroidService);
+        neoApp.run();
+    }
+
+    /*
+     * Method to run whole application.
+     */
+    private void run() {
         try {
             Runtime.getRuntime().addShutdownHook(new Thread(() -> System.out.println("\n\nFinished work.")));
-
-            trustAllCerts();
 
             String formattedToday = format.format(Calendar.getInstance().getTime());
 
@@ -112,5 +80,15 @@ public class NEOApp {
             System.out.println(String.format("The execution failed due to the following reason: %s", e.getMessage()));
             e.printStackTrace(System.err);
         }
+    }
+
+    /**
+     * Method to print out url response.
+     *
+     * @param content - byte array to print.
+     */
+    private static void printURLResponce(byte[] content) {
+        System.out.println("/***** URL Content *****\n");
+        System.out.println(new String(content));
     }
 }
--- src/com/nasa/app/services/NasaService.java
+++ src/com/nasa/app/services/NasaService.java
@@ -1,9 +1,15 @@
 package com.nasa.app.services;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.security.GeneralSecurityException;
+import java.security.cert.X509Certificate;
 
 public class NasaService {
 
@@ -14,6 +20,7 @@ public class NasaService {
      * @param url - url object
      */
     public byte[] getPageContent(String url) {
+        trustAllCerts();
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         try {
             URL urlObj = new URL(url);
@@ -33,4 +40,33 @@ public class NasaService {
 
         return outputStream.toByteArray();
     }
+
+    /**
+     * Method to trust all certificates, to prevent error connected with URL connection to https.
+     */
+    private void trustAllCerts() {
+        TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[0];
+                    }
+
+                    public void checkClientTrusted(
+                            X509Certificate[] certs, String authType) {
+                    }
+
+                    public void checkServerTrusted(
+                            X509Certificate[] certs, String authType) {
+                    }
+                }
+        };
+
+        try {
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (GeneralSecurityException e) {
+        }
+    }
+
 }
